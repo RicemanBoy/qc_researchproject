4f5b758ac4006d7dc4621b95d17a43039dca9e83
Dustin S <dustin.seboldt@fau.de>
2024-11-08 13:50:24 +0100

    hmm

---
+++ RP_QEC_1stIdeal/generator.py
@@ -0,0 +1,27 @@
+N=200
+
+
+for i in range(N):
+    file=open('{}.py'.format(i), 'a')
+    file.write('from functions import *\n')
+    file.write('gen_data({},0.3,100,50)'.format(i))
+    file.close()
+
+for i in range(N):
+    file=open('job{}.script'.format(i),'a')
+    file.write('#!/bin/bash -l \n')
+    file.write('#SBATCH --ntasks=3 \n')
+    file.write('#SBATCH --time=02:00:00 \n' )
+    file.write('#SBATCH --job-name=IQPE_QEC{} \n'.format(i))
+    file.write('#SBATCH --export=NONE \n')
+    file.write('unset SLURM_EXPORT_ENV \n')
+    file.write('module load python/3.10-anaconda \n')
+    file.write('python3 {}.py'.format(i)) 
+    file.close()
+
+file=open('sc.script','a')
+file.write('#! /bin/bash')
+for i in range(N):
+    file.write('\n sbatch.tinyfat job{}.script'.format(i))
+file.close()
+
